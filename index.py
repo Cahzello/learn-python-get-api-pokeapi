1b1d0fb801446b154db93ded1bf7bb023ddaf51d
Cahzello <rizkyrasya.m@gmail.com>
2024-11-03 19:35:43 +0700

    fix; add try catch block

--- index.py
+++ index.py
@@ -7,13 +7,21 @@ def get_pokemon_data(pokemon_name):
     print(f"Getting data for {pokemon_name}...")
     print("Fetching data from PokeAPI...")
     url = f"https://pokeapi.co/api/v2/pokemon/{pokemon_name.lower()}"
-    response = requests.get(url)
+    try:
+        response = requests.get(url)
+    except:
+        print("Error: No internet");    
     if response.status_code == 200:
         data = response.json()
-        with open(f"{pokemon_name.lower()}.json", "w") as json_file:
-            json.dump(data, json_file)
-        print(f"Data saved to {pokemon_name.lower()}.json")
-        return True
+        try:
+            with open(f"{pokemon_name.lower()}.json", "w") as json_file:
+                json.dump(data, json_file)
+        except:
+            print("Error: Could not write to file")
+            return False
+        finally:
+            print(f"Data saved to {pokemon_name.lower()}.json")
+            return True
     else:
         print(f"Failed to fetch data. Status code: {response.status_code}")
         return False
