b326447ba7ea55354988857143615cc302c91d8d
Cahzello <rizkyrasya.m@gmail.com>
2024-11-03 19:38:30 +0700

    fix: inden

--- index.py
+++ index.py
@@ -10,21 +10,22 @@ def get_pokemon_data(pokemon_name):
     try:
         response = requests.get(url)
     except:
-        print("Error: No internet");    
-    if response.status_code == 200:
-        data = response.json()
-        try:
-            with open(f"{pokemon_name.lower()}.json", "w") as json_file:
-                json.dump(data, json_file)
-        except:
-            print("Error: Could not write to file")
+        print("Error: No internet")
+    finally:    
+        if response.status_code == 200:
+            data = response.json()
+            try:
+                with open(f"{pokemon_name.lower()}.json", "w") as json_file:
+                    json.dump(data, json_file)
+            except:
+                print("Error: Could not write to file")
+                return False
+            finally:
+                print(f"Data saved to {pokemon_name.lower()}.json")
+                return True
+        else:
+            print(f"Failed to fetch data. Status code: {response.status_code}")
             return False
-        finally:
-            print(f"Data saved to {pokemon_name.lower()}.json")
-            return True
-    else:
-        print(f"Failed to fetch data. Status code: {response.status_code}")
-        return False
 
 # Example usage:
 get_pokemon_data(input("Masukan nama pokemon: "))
